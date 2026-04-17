0108951ffd25d8c42345a07eacf6ee7e979e3cc2
Noah F <114858797+no8f@users.noreply.github.com>
2026-04-17 11:06:32 +0200

    init

---
+++ Source/HamsterRun/Private/Test/TestCube.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Test/TestCube.h"
+
+// Sets default values
+ATestCube::ATestCube()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATestCube::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATestCube::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	double speed = 20.f;
+	
+	auto loc = GetActorLocation();
+	loc.X += 10 * DeltaTime;
+	
+	SetActorLocation(loc);
+
+}
+
