9d9416a37ebcc6ddebccea42f729d3595527d136
Noah F <114858797+no8f@users.noreply.github.com>
2026-04-20 14:03:39 +0200

    PA4-24 local multiplayer (#1)
    * added local muliplayer
    
    * aaaaaaaaaaaaaaaa
    
    * Applied Fixes to Local Multiplayer Code
    
    ---------
    
    Co-authored-by: Spielemeistr <mppb1908@gmx.de>

---
+++ Source/HamsterRun/Private/Interfaces/InputConnection.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/InputConnection.h"
+
+// Add default functionality here for any IInputConnection functions that are not pure virtual.
