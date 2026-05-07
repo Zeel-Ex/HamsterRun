39eecf83dc156b0402d91b8e06e3874e26e83028
Noah F <114858797+no8f@users.noreply.github.com>
2026-05-07 18:52:29 +0200

    switched to polling for lobby list

---
+++ Source/HamsterRun/Public/Game/RepNetPlayerController.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "RepNetPlayerController.generated.h"
+
+/**
+ * 
+ */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPlayerStateChanged, APlayerState*, NewPlayerState);
+
+UCLASS()
+class HAMSTERRUN_API ARepNetPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	virtual void OnRep_PlayerState() override;
+	
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnPlayerStateChanged(APlayerState* NewPlayerState);
+};
