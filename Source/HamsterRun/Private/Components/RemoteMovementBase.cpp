87a6019531542f9daccd800d7e6384dee5b27975
Spielemeistr <mppb1908@gmx.de>
2026-05-09 13:53:20 +0200

    Fixed Apply to Different Net Modes

---
+++ Source/HamsterRun/Private/Components/RemoteMovementBase.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Components/RemoteMovementBase.h"
+
+// Sets default values for this component's properties
+URemoteMovementBase::URemoteMovementBase()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+	
+}
+
+
+// Called when the game starts
+void URemoteMovementBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+// Called every frame
+void URemoteMovementBase::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+}
+
+CurrentNetMode URemoteMovementBase::GetCurrentNetMode() const
+{
+	if (const UWorld* World = GetWorld())
+	{
+		return static_cast<CurrentNetMode>(World->GetNetMode());
+	}
+	return Standalone;
+}
