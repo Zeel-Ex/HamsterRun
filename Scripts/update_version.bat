7717b23ea553d05bfc58d9b75b755065d19d8a5c
no8f <snipez5002@gmail.com>
2026-07-05 12:11:44 +0200

    version bump

---
+++ Scripts/update_version.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set PROJDIR=%~1
+for /f %%i in ('git -C "%PROJDIR%" rev-parse --short HEAD') do set HASH=%%i
+
+set INIFILE=%PROJDIR%\Config\DefaultGame.ini
+
+powershell -Command ^
+  "(Get-Content '%INIFILE%') -replace '^ProjectVersion=.*', 'ProjectVersion=0.1.0-%HASH%' | Set-Content '%INIFILE%'"
