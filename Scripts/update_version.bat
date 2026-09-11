68151436cfa31d500e9b6a8a0597a39ff51fb9df
ShadowWolve14 <jonas.hever@icloud.com>
2026-09-11 19:03:27 +0200

    PA4-200 0.3.2 -> 0.3.3

--- Scripts/update_version.bat
+++ Scripts/update_version.bat
@@ -7,4 +7,4 @@ for /f %%i in ('git -C "%PROJDIR%" rev-parse --short HEAD') do set HASH=%%i
 set INIFILE=%PROJDIR%\Config\DefaultGame.ini
 
 powershell -Command ^
-  "(Get-Content '%INIFILE%') -replace '^ProjectVersion=.*', 'ProjectVersion=0.3.2-%HASH%' | Set-Content '%INIFILE%'"
+  "(Get-Content '%INIFILE%') -replace '^ProjectVersion=.*', 'ProjectVersion=0.3.3-%HASH%' | Set-Content '%INIFILE%'"
